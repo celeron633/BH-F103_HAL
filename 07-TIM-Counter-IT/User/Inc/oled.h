1360ae96dbbc0c85b9ba3f4a5eb65e5f2f7554a9
celeron633 <dengxionghui@hotmail.com>
2024-08-31 13:52:21 +0800

    TIM-Counter-IT: initial commit

---
+++ 07-TIM-Counter-IT/User/Inc/oled.h
@@ -0,0 +1,36 @@
+#ifndef __OLED_H__
+#define __OLED_H__
+
+#include "main.h"
+#include "i2c.h"
+#include "stm32f1xx_hal_uart.h"
+#include "stm32f103xe.h"
+
+extern struct OLED_Config g_oledCfg;
+
+struct OLED_Config {
+    I2C_HandleTypeDef *i2cHandle;
+    uint8_t i2cAddr;
+};
+
+void OLED_ConfigDisplay(I2C_HandleTypeDef *handle, uint8_t i2cAddr);
+int  OLED_InitDisplay();
+
+void OLED_SetPos(uint8_t X, uint8_t Y);
+void OLED_SetCursor(uint8_t page, uint8_t X);
+void OLED_Fill(uint8_t data);
+
+void OLED_NewFrame();
+void OLED_ShowFrame();
+void OLED_SetPixel(int X, int Y);
+
+void OLED_ShowImage(int16_t X, int16_t Y, uint8_t width, uint8_t height, const uint8_t *image);
+
+void OLED_ShowChar(uint8_t X, uint8_t Y, char c);
+void OLED_ShowString(uint8_t X, uint8_t Y, const char *str);
+
+
+// TEST
+void OLED_Test();
+
+#endif
