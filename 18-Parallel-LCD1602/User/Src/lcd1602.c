d127250abe60e9fbff68f2a72acd43a5989cfba7
celeron633 <dengxionghui@hotmail.com>
2024-09-29 14:26:14 +0800

    Parallel-LCD1602: fix 4-bit mode 1. learn the code from https://www.youtube.com/watch?v=ITTBWSQTi3c, big    thanks to the author

--- 18-Parallel-LCD1602/User/Src/lcd1602.c
+++ 18-Parallel-LCD1602/User/Src/lcd1602.c
@@ -3,7 +3,25 @@
 #include "delay.h"
 
 #define DATA_GPIO_Port GPIOA
-#define DATA_8BIT_MODE
+#define DATA_4BIT_MODE
+
+#define RS_Pin GPIO_PIN_9
+#define RS_GPIO_Port GPIOB
+#define RW_Pin GPIO_PIN_10
+#define RW_GPIO_Port GPIOB
+#define EN_Pin GPIO_PIN_11
+#define EN_GPIO_Port GPIOB
+#define D4_Pin GPIO_PIN_4
+#define D4_GPIO_Port GPIOA
+#define D5_Pin GPIO_PIN_5
+#define D5_GPIO_Port GPIOA
+#define D6_Pin GPIO_PIN_6
+#define D6_GPIO_Port GPIOA
+#define D7_Pin GPIO_PIN_7
+#define D7_GPIO_Port GPIOA
+
+// from delay.h us level delay
+#define delay(i) delay_us(i)
 
 static inline void LCD_RS_L()
 {
@@ -35,6 +53,7 @@ static inline void LCD_EN_H()
     HAL_GPIO_WritePin(LCD1602_EN_GPIO_Port, LCD1602_EN_Pin, GPIO_PIN_SET);
 }
 
+#ifdef DATA_8BIT_MODE
 static inline void LCD_W_Data(uint8_t d)
 {
     // 直接写输出数据寄存器操作bit4-bit7
@@ -52,19 +71,13 @@ static int LCD_GetBusyStatus()
     return status;
 }
 
-void LCD_delay(void)
-{
-    HAL_Delay(1);
-}
-
-#ifdef DATA_8BIT_MODE
 void LCD_SendCmd(uint8_t cmd)
 {
     LCD_RS_L();
     LCD_RW_L();
     LCD_W_Data(cmd);
     LCD_EN_H();
-    LCD_delay();
+    delay(20);
     LCD_EN_L();
 }
 
@@ -75,49 +88,45 @@ void LCD_SendData(uint8_t data)
     LCD_RW_L();
     LCD_W_Data(data);
     LCD_EN_H();
-    LCD_delay();
+    delay(20);
     LCD_EN_L();
 }
 #else
+void LCD_Send(uint8_t data, int rs)
+{
+    HAL_GPIO_WritePin(RS_GPIO_Port, RS_Pin, rs);
+
+    // write bit to pin
+    HAL_GPIO_WritePin(D7_GPIO_Port, D7_Pin, (data>>3)&0x01);
+    HAL_GPIO_WritePin(D6_GPIO_Port, D6_Pin, (data>>2)&0x01);
+    HAL_GPIO_WritePin(D5_GPIO_Port, D5_Pin, (data>>1)&0x01);
+    HAL_GPIO_WritePin(D4_GPIO_Port, D4_Pin, (data>>0)&0x01);
+
+    // flush to memory while en falling edge
+    HAL_GPIO_WritePin(EN_GPIO_Port, EN_Pin, 1);
+    delay(20);
+    HAL_GPIO_WritePin(EN_GPIO_Port, EN_Pin, 0);
+    delay(20);
+}
 
 void LCD_SendCmd(uint8_t cmd)
 {
-    LCD_RS_L();
-    LCD_RW_L();
+    uint8_t data2Send;
+    data2Send = (cmd >> 4) & 0x0F;
+    LCD_Send(data2Send, 0);
 
-    uint8_t i = 0;
-    i = cmd & 0xF0;
-    LCD_W_Data(i|0);
-    LCD_EN_H();
-    LCD_delay();
-    LCD_EN_L();
-
-    i = cmd << 4;
-    i &= 0xF0;
-    LCD_W_Data(i);
-    LCD_EN_H();
-    LCD_delay();
-    LCD_EN_L();
+    data2Send = cmd & 0x0F;
+    LCD_Send(data2Send, 0);
 }
 
 void LCD_SendData(uint8_t data)
 {
-    LCD_RS_H();
-    LCD_RW_L();
+    uint8_t data2Send;
+    data2Send = (data >> 4) & 0x0F;
+    LCD_Send(data2Send, 1);
 
-    uint8_t i = 0;
-    i = data & 0xF0;
-    LCD_W_Data(i|0);
-    LCD_EN_H();
-    LCD_delay();
-    LCD_EN_L();
-
-    i = data << 4;
-    i &= 0xF0;
-    LCD_W_Data(i);
-    LCD_EN_H();
-    LCD_delay();
-    LCD_EN_L();
+    data2Send = data & 0x0F;
+    LCD_Send(data2Send, 1);
 }
 #endif
 
@@ -132,18 +141,33 @@ void LCD_Init()
     LCD_SendCmd(0x0F);
 #else
     // 4-bit
-    LCD_delay();
+    // init 4-bit mode
+    HAL_Delay(50);
+    LCD_SendCmd(0x30);
+    HAL_Delay(5);
+    LCD_SendCmd(0x30);
+    HAL_Delay(1);
+    LCD_SendCmd(0x30);
+    HAL_Delay(10);
+    LCD_SendCmd(0x20);
+    HAL_Delay(10);
+
+    // init display
     LCD_SendCmd(0x28);
+    HAL_Delay(1);
+    LCD_SendCmd(0x08);
+    HAL_Delay(1);
     LCD_SendCmd(0x01);
+    HAL_Delay(1);
     LCD_SendCmd(0x06);
-    LCD_SendCmd(0x0F);
+    HAL_Delay(1);
+    LCD_SendCmd(0x0C);
+    HAL_Delay(1);
 #endif
 }
 
 void LCD_ShowString(const char *str)
 {
     while (*str)
-    {
         LCD_SendData(*str++);
-    }
 }
